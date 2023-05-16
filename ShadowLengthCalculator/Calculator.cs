b08b5a8d882618304d0fa344da3e70cf6c11479a
dunno <dunnotoru@yandex.ru>
2023-05-16 21:14:50 +0700

    Добавьте файлы проекта.

---
+++ ShadowLengthCalculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowLengthCalculator
+{
+    public struct Segment
+    {
+        public int StartCoord;
+        public int EndCoord;
+
+        public Segment(int startCoord,int endCoord)
+        {
+            StartCoord = startCoord;
+            EndCoord = endCoord;
+        }
+    }
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public int Calc(List<Segment> segments)
+        {
+            int shadowLength = 0;
+            segments.OrderBy(x => x.StartCoord);
+
+            shadowLength += segments[0].EndCoord - segments[0].StartCoord;
+            for (int i = 1; i < segments.Count - 1; i++)
+            {
+                if (segments[i + 1].StartCoord < segments[i].StartCoord)
+                    shadowLength += segments[i + 1].EndCoord - segments[i].StartCoord;
+                else if (segments[i + 1].StartCoord > segments[i].EndCoord)
+                    shadowLength += segments[i + 1].EndCoord - segments[i].StartCoord;
+            }
+
+            return shadowLength;
+        }
+    }
+
+    
+}
---
+++ ShadowLengthCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShadowLengthCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(10,30),
+                new Segment(20,40),
+                new Segment(50,70),
+                new Segment(60,90)
+            };
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Calc(segments));
+        }
+    }
+}
