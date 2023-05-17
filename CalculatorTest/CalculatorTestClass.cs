c10c15c49b6b92745116abfc96eecc951e419980
Mihei03 <mikhail.lol.03@gmail.com>
2023-05-17 20:24:54 +0700

    Добавление тестов

--- CalculatorTest/CalculatorTestClass.cs
+++ CalculatorTest/CalculatorTestClass.cs
@@ -21,7 +21,7 @@ namespace CalculatorTest
             double expected = 90;
             Calculator calculator = new Calculator();
             double actual = calculator.Calc(segments);
-            
+
             Assert.AreEqual(expected, actual);
         }
 
@@ -32,7 +32,7 @@ namespace CalculatorTest
             {
                 new Segment(-50,-30),
                 new Segment(-10, 10),
-                new Segment(10,40),
+                new Segment(0,40),
             };
 
             double expected = 70;
@@ -50,6 +50,7 @@ namespace CalculatorTest
                 new Segment(1,4),
                 new Segment(3, 7),
                 new Segment(10,15),
+                new Segment(13,15),
             };
 
             double expected = 11;
@@ -64,15 +65,161 @@ namespace CalculatorTest
         {
             List<Segment> segments = new List<Segment>()
             {
-                new Segment(4,1),
+                new Segment(13,15),
+                new Segment(10,15),
                 new Segment(3, 7),
-                new Segment(15,10),
+                new Segment(1,4),
             };
 
+            double expected = 11;
             Calculator calculator = new Calculator();
             double actual = calculator.Calc(segments);
 
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest5()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(4,1),
+                new Segment(3, 7),
+                new Segment(15,10),
+                new Segment(13,15),
+            };
+
             double expected = 11;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest6()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(-50,-30),
+                new Segment(-40, -10),
+                new Segment(-10,10),
+                new Segment(1,1),
+                new Segment(10,40),
+            };
+
+            double expected = 90;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest7()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(-3.5,-2.5),
+                new Segment(-2, -1),
+                new Segment(-1.5,0),
+                new Segment(1,3),
+            };
+
+            double expected = 5;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest8()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(-2.5,-3.5),
+                new Segment(-2, -1),
+                new Segment(0,-1.5),
+                new Segment(3,1),
+            };
+
+            double expected = 5;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest9()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(-2, -1),
+                new Segment(1,3),
+                new Segment(-1.5,0),
+                new Segment(-3.5,-2.5),
+            };
+
+            double expected = 5;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest10()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(-3.5, -2.5),
+                new Segment(-3,-1.5),
+                new Segment(-2,1.5),
+                new Segment(0,0),
+            };
+
+            double expected = 5;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest11()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(-3.5, -2.5),
+                new Segment(20,20),
+                new Segment(-2,1.5),
+                new Segment(-3,-1.5),
+            };
+
+            double expected = 5;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcTest12()
+        {
+            List<Segment> segments = new List<Segment>()
+            {
+                new Segment(-3.5, -2.5),
+                new Segment(-40,-40),
+                new Segment(-2,1.5),
+                new Segment(-3,-1.5),
+            };
+
+            double expected = 5;
+            Calculator calculator = new Calculator();
+            double actual = calculator.Calc(segments);
+
             Assert.AreEqual(expected, actual);
         }
     }
